4bfe87659001482d0f0df5e8324ad8ff51fd4218
HanhLTM-OU <55686467+HanhLTM-OU@users.noreply.github.com>
2020-05-29 15:54:47 +0700

    update layout bat dau 2

--- app/src/main/java/com/example/mytheduc/Activity/BatDauActivity.java
+++ app/src/main/java/com/example/mytheduc/Activity/BatDauActivity.java
@@ -1,10 +1,12 @@
 package com.example.mytheduc.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,11 +18,21 @@ public class BatDauActivity extends Activity  {
 
     /*private ListView lvBatDau;
     private ArrayList <String> list;*/
+    private Button btn_Di;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_batdau);
+        btn_Di = (Button) findViewById(R.id.btnDi);
+        btn_Di.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BatDauActivity.this, SanSangActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
         /*lvBatDau = (ListView) findViewById(R.id.listBaiTap);
         list = new ArrayList<String>();
         list.add("Bài 1");
---
+++ app/src/main/java/com/example/mytheduc/Activity/SanSangActivity.java
@@ -0,0 +1,14 @@
+package com.example.mytheduc.Activity;
+
+        import android.os.Bundle;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import com.example.mytheduc.R;
+
+public class SanSangActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sansang);
+    }
+}

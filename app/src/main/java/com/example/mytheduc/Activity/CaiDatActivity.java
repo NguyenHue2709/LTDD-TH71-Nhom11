ca66c8f4244da9c66f8cb611cffd1168a6de5a9a
NguyenHue2709 <1751012028hue@ou.edu.vn>
2020-05-30 18:59:49 +0700

    Cài đặt - Vòng Lặp Lại

--- app/src/main/java/com/example/mytheduc/Activity/CaiDatActivity.java
+++ app/src/main/java/com/example/mytheduc/Activity/CaiDatActivity.java
@@ -1,14 +1,23 @@
 package com.example.mytheduc.Activity;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import com.example.mytheduc.R;
 
 public class CaiDatActivity extends AppCompatActivity {
+
+    private Button numberPicker;
+    private TextView Number;
 
 
     @Override
@@ -21,6 +30,15 @@ public class CaiDatActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Cài đặt");
 
+        numberPicker = (Button) findViewById(R.id.btn_DialogNumberPicker);
+        Number = (TextView) findViewById(R.id.txt_Number);
+        numberPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberPickerDialog();
+            }
+        });
+
     }
 
     @Override
@@ -30,6 +48,36 @@ public class CaiDatActivity extends AppCompatActivity {
             this.finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void numberPickerDialog(){
+        NumberPicker myNumberPicker = new NumberPicker(this);
+        myNumberPicker.setMaxValue(4);
+        myNumberPicker.setMinValue(1);
+        NumberPicker.OnValueChangeListener myValueChangeListener = new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                Number.setText(""+newVal+"  Lần");
+            }
+        };
+        myNumberPicker.setOnValueChangedListener(myValueChangeListener);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this).setView(myNumberPicker);
+        builder.setTitle("Đặt lại vòng lặp");
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
+
     }
 
 }
